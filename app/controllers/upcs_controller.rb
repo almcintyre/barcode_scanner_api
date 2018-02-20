56b0883b7f7e75c8c25b7e28077c7347f5dfaeb7
almcintyre <almcintyre.eng@gmail.com>
2018-02-20 11:24:59 -0800

    Adds form submission

--- app/controllers/upcs_controller.rb
+++ app/controllers/upcs_controller.rb
@@ -1,5 +1,4 @@
 class UpcsController < ApplicationController
-  before_action :set_todo, only: [:show, :update, :destroy]
 
  # GET /todos
  def index
@@ -15,7 +14,7 @@ class UpcsController < ApplicationController
 
  # GET /todos/:id
  def show
-   json_response(@upc)
+   render template: "upcs/index"
  end
 
  # PUT /todos/:id
@@ -29,15 +28,11 @@ class UpcsController < ApplicationController
    @upc.destroy
    head :no_content
  end
- 
+
   private
 
   def upc_params
     # whitelist params
     params.permit(:upc, :product_name)
-  end
-
-  def set_todo
-    @upc = Upc.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :upcs do
   end
+  root 'upcs#show'
+  post 'upcs/create', to: "upcs#create"
+  get 'upcs/index', to: "upcs#index"
 end
